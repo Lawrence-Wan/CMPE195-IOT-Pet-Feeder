aed334213e9b7d92486da2bedb5b781f3a8de2aa
Toan Nguyen <dudewitbow@gmail.com>
2018-11-26 15:03:04 -0800

    weight changes and commented logic for scale section

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #include "Projects/Drivers/RFID/Rfid.hpp"
 #include "Projects/Drivers/Servo.hpp"
 #include "clients/FeederSettings.h"
+#include "Projects/Drivers/Weight/Feeder.cpp"
+#include "Projects/Drivers/Weight/MCP3008SPI.cpp"
 
 using namespace std;
 
@@ -44,6 +46,10 @@ int main(void){
 //**Initialize Servos, default bowl as open - do not dispense
 
 //**Initialize Scale, set initial weight
+    feeder feed1(0); //initialize feeder1 to MCP3008 port 0
+    feed1.tare(); //zero out scale
+    double weightval = 0; //for weight scale usage
+    double tempval = 0; //for if logic
 
 //**Initialize RFID
     rfid_control.Initialize();
@@ -67,9 +73,26 @@ int main(void){
 //**check scale amount - 3 scenarios
         //1 - MUST - when check_weight is true record current weight, set ok_to_feed and allow feeder to 
             //dispense food, check new weight, combine and send to server, clear check_weight
+        /*
+        if(check_weight == True){
+            ok_to_feed = True;
+            weightval = feed1.measure();
+            //combine and send to server
+            check_weight = False;
+        }
+        */
         //2 - MUST - when a change in weight is detected, wait 15 seconds and compare before and after, 
             //record change and send to server
+        /*
+        if(tempval < weightval-10 || tempval > weightval + 10){//if the current weight is at least 10 grams difference(tweak range for sensitivity in scale, fluctuates a lot)
+            weightval = feed1.measure();
+            //combine and send to server
+        }
+        */
         //3 - periodically check to maintain current weight, should not have significant changes
+        /*
+        tempval = feed1.measure();
+        */
 
 //**check impeller servo logic based on ok_to_feed flag and dispense_amount, clear ok_to_feed
 
